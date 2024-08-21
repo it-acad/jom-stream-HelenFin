1b3beb64ef7a969098e65bda187d4a092d1e33da
HelenFin <personal.lena@gmail.com>
2024-08-21 08:20:59 +0200

    add task 5

--- src/main/java/jom/com/softserve/s6/task5/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task5/MyUtils.java
@@ -7,6 +7,21 @@ import java.util.stream.Stream;
 
 public class MyUtils {
 
-    //Write your code here
+    public Stream<String> nameList(Map<String, Stream<String>> map) {
+        if (map == null) {
+            throw new NullPointerException("Map cannot be null");
+        }
+
+        return map.values().stream()
+                .flatMap(s -> s)
+                .filter(Objects::nonNull)
+                .map(String::trim)
+                .filter(s -> !s.isEmpty())
+                .map(s -> s.replaceAll("\\s+", ""))
+                .map(String::toLowerCase)
+                .distinct()
+                .sorted()
+                .map(s -> Character.toUpperCase(s.charAt(0)) + s.substring(1));
+    }
 
 }
