96314b66c82ed301a0a1051c7ffb7a97be3b7198
HelenFin <personal.lena@gmail.com>
2024-08-21 08:17:45 +0200

    add task 2

--- src/main/java/jom/com/softserve/s6/task2/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task2/MyUtils.java
@@ -1,12 +1,15 @@
 package jom.com.softserve.s6.task2;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 public class MyUtils {
     public static String getDateAfterToday(int years, int months, int days) {
-        //Write code there
-        return "";
+        LocalDate localDate = LocalDate.now();
+        Period period = Period.of(years, months, days);
+        LocalDate newDate = localDate.plus(period);
+        return newDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
