17116a4a998f7abb4f02d9cd471704965945c0e0
HelenFin <personal.lena@gmail.com>
2024-08-21 08:15:19 +0200

    add task 6

--- src/main/java/jom/com/softserve/s6/task6/MyUtils.java
+++ src/main/java/jom/com/softserve/s6/task6/MyUtils.java
@@ -2,13 +2,37 @@ package jom.com.softserve.s6.task6;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.Objects;
+
+
 
 public class MyUtils {
+    public Map<String, Stream<String>> phoneNumbers(List<Stream<String>> list){
 
-    //Write your code here
-
+        return list.stream()
+                .flatMap(s -> s)
+                .distinct()
+                .filter(Objects::nonNull)
+                .map(String::trim)
+                .filter(phone -> !phone.isEmpty())
+                .map(phone -> phone.replaceAll("[\\s\\-()]", ""))
+                .collect(Collectors.groupingBy(phone -> {
+                    if (phone.length() < 7) {
+                        return "err";
+                    } else if (phone.startsWith("0")) {
+                        return phone.substring(0, 3);
+                    } else {
+                        return "loc";
+                    }
+                }, Collectors.mapping(phone -> {
+                    if (phone.startsWith("0") && phone.length() > 3) {
+                        return phone.substring(3);
+                    } else {
+                        return phone;
+                    }
+                }, Collectors.collectingAndThen(Collectors.toList(), listToSort -> listToSort.stream().sorted()))));
+    }
 }
+
